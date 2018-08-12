bf00b9e63a2b122f943daa4b758b70e73ff2524b
Brandon Hill <brandon@branhill.com>
2018-08-12 21:15:13 +0800

    Add projects

---
+++ src/EdgeRedirector.Gui/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+
+namespace EdgeRedirector.Gui
+{
+    public class EntryPoint
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new Application();
+            app.Run(new SettingsWindow());
+        }
+    }
+}
---
+++ src/EdgeRedirector/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EdgeRedirector
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                Redirect(args[0]);
+            }
+            else
+            {
+                Gui.EntryPoint.Main();
+            }
+        }
+
+        private static void Redirect(string url)
+        {
+
+        }
+    }
+}
