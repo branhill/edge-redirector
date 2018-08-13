ec0b2e62b0e555d4fd1c1e82f139ea46e358ec1f
Brandon Hill <brandon@branhill.com>
2018-08-13 10:24:48 +0800

    Add settings class

---
+++ src/EdgeRedirector.Core/Settings.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace EdgeRedirector.Core
+{
+    public class Settings
+    {
+        private static readonly JsonSerializer JsonSerializer = new JsonSerializer { Formatting = Formatting.Indented };
+
+        public string Browser { get; set; }
+
+        public string SearchEngine { get; set; }
+
+        public static Settings Open(string path)
+        {
+            if (!File.Exists(path))
+                return new Settings();
+
+            using (var streamReader = new StreamReader(path))
+            using (var jsonTextReader = new JsonTextReader(streamReader))
+            {
+                return JsonSerializer.Deserialize<Settings>(jsonTextReader);
+            }
+        }
+
+        public void Save(string path)
+        {
+            using (var streamWriter = new StreamWriter(path))
+            using (var jsonTextWriter = new JsonTextWriter(streamWriter))
+            {
+                JsonSerializer.Serialize(jsonTextWriter, this);
+            }
+        }
+    }
+}
